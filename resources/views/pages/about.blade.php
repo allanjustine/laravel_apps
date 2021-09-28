fd553ab4150e97613fd6bf8e419f2fee8ddc8c28
allanjustine <labya200@gmail.com>
2021-09-28 14:28:46 +0800

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+    <title>About</title>
+</head>
+<body>
+    <h1>About</h1>
+    <a href="{{url('/')}}"> Home </a>| <a href="{{url('/profile')}}">Profile</a>
+    | About
+</body>
+</html>
---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+    <title>Profile</title>
+</head>
+<body>
+    <h1>Profile</h1>
+    <a href="{{url('/')}}"> Home </a>| Profile
+    | <a href="{{url('/about')}}">About</a>
+</body>
+</html>
